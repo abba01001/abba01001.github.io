59f6f01c35924a3fdb97419c97619a6cc49f392e
abba01001 <1324058099@qq.com>
2024-11-10 03:31:56 +0800

    Automated commit

--- 更新博客.bat
+++ 更新博客.bat
@@ -1,13 +1,22 @@
 @echo off
-:: 添加所有更改
-git add .
 
-:: 提交更改
-git commit -m "Automated commit"
+:: 检查当前所有本地分支
+for /f "tokens=*" %%i in ('git branch --list') do (
 
-:: 推送所有分支的更改到远程仓库
-git push --all
+    :: 切换到每个分支
+    git checkout %%i
 
-:: 打印成功消息
-echo Push completed successfully!
+    :: 添加所有更改
+    git add .
+
+    :: 提交更改
+    git commit -m "Automated commit"
+
+    :: 推送更改到远程仓库
+    git push origin %%i
+
+)
+
+:: 提示操作已完成
+echo All branches have been pushed successfully!
 pause
